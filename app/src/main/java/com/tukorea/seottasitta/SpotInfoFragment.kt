34a7c569b2b3a8a2fc4e3f1f3a1dede87cd180a3
kevinkim <nuokim@tukorea.ac.kr>
2023-12-13 13:24:22 +0900

    Beta v0.1 fix bug

--- app/src/main/java/com/tukorea/seottasitta/SpotInfoFragment.kt
+++ app/src/main/java/com/tukorea/seottasitta/SpotInfoFragment.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SpotInforFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SpotInforFragment : Fragment() {
+class SpotInfoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class SpotInforFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SpotInforFragment().apply {
+            SpotInfoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
